65b5c4f779e4c5e7ce86e9d4508319b4c6e0162c
1MP-Group <info@mpgroup.org>
2026-03-07 12:02:39 -0500

    Update part-d.js

--- part-d.js
+++ part-d.js
@@ -1,5 +1,5 @@
 function calculatePartD() {
-    const basePremium = 38.99; // 2026 value
+    const basePremium = 36.78; // 2025 value
     const uncoveredMonths = parseInt(document.getElementById('partD-months').value);
 
     if (isNaN(uncoveredMonths)) {
